d3320ffecc3432e3a0c6690c8f0c420ad946164f
Dev Sai <63507117+devsai9@users.noreply.github.com>
2025-01-03 18:07:02 -0500

    Create script.js

---
+++ 5.1/script.js
@@ -0,0 +1,32 @@
+let menu = document.getElementById('menu');
+menu.style.opacity = 0;
+
+let mouseX = 10;
+let mouseY = 60;
+
+function showMenu(e) {
+    menu.style.opacity = 1;
+    menu.classList.remove('hidden');
+    menu.style.position = "absolute";
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    menu.style.left = mouseX + "px";
+    menu.style.top = mouseY + "px";
+}
+
+function hideMenu() {
+    menu.style.left = (mouseX - 8) + "px";
+    menu.style.top = mouseY + "px";
+    menu.style.opacity = 0;
+    menu.classList.add('hidden');
+    menu.style.position = "relative";
+}
+
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    showMenu(e);
+});
+
+document.addEventListener('click', (e) => {
+    hideMenu();
+});
